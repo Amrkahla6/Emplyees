04e67d87029e3ef355a321d37a017c759ec325f6
Amr Kahla <amrkahla6@gmail.com>
2026-06-23 16:41:40 +0300

    Impelement Swagger & data validation

--- src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
+++ src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
@@ -1,12 +1,19 @@
 package com.luv2code.springboot.employees.request;
 
+import jakarta.validation.constraints.*;
+
 public class EmployeeRequest {
 
-
+    @NotBlank(message = "First name is mandatory")
+    @Size(min=2, max=50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min=2, max=50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
     public EmployeeRequest() {
