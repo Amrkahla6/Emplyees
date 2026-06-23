25309fa16949f82f8300061eb73082b86ea1e245
Amr Kahla <amrkahla6@gmail.com>
2026-06-23 16:33:52 +0300

    Service Layer Enhancement

--- src/main/java/com/luv2code/springboot/employees/service/EmployeeService.java
+++ src/main/java/com/luv2code/springboot/employees/service/EmployeeService.java
@@ -1,11 +1,16 @@
 package com.luv2code.springboot.employees.service;
 
 import com.luv2code.springboot.employees.entity.Employee;
+import com.luv2code.springboot.employees.request.EmployeeRequest;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> findAll();
-
+    Employee findById(long id);
+    Employee save(EmployeeRequest theEmployeeRequest);
+    Employee update(Long id,EmployeeRequest theEmployeeRequest);
+    Employee convertToEmployee(Long id, EmployeeRequest theEmployeeRequest);
+    void deleteById(long id);
 }
--- src/main/java/com/luv2code/springboot/employees/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/employees/service/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.employees.service;
 
 import com.luv2code.springboot.employees.dao.EmployeeDao;
 import com.luv2code.springboot.employees.entity.Employee;
+import com.luv2code.springboot.employees.request.EmployeeRequest;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,35 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(long id) {
+        return employeeDao.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(EmployeeRequest theEmployeeRequest) {
+        Employee employee = convertToEmployee(0L,theEmployeeRequest);
+        return employeeDao.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public Employee update(Long id, EmployeeRequest theEmployeeRequest) {
+        Employee employee = convertToEmployee(id,theEmployeeRequest);
+        return employeeDao.save(employee);
+    }
+
+    @Override
+    public Employee convertToEmployee(Long id, EmployeeRequest theEmployeeRequest) {
+        return new Employee(id,theEmployeeRequest.getFirstName(),theEmployeeRequest.getLastName(),theEmployeeRequest.getEmail());
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        employeeDao.deleteById(id);
     }
 }
